638f1b4a786e9c956c2da250685ad6fa92befc83
Sergay <tsvetkoffgolod@yandex.ru>
2022-03-15 00:14:18 +0300

    burger menu create stap 1

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,25 +1,48 @@
 import React from 'react';
 import './Navigation.css';
 import { Link } from 'react-router-dom';
+import img from '../../images/burggger.svg'
 
 function Navigation({isLogin}) {
+
+  const [isBurgerMenu, setBurgerMenu] = React.useState(false);
+
+  function handelBurgerMenu() {
+    setBurgerMenu(true);
+  }
+
   return (
     <nav className='nav'>
       <ul className='nav__list'>
 
         {isLogin ?
           <>
+          <div className='navigate-wrapper'>
             <li className='nav__item-log'><Link className='nav__link' to="/movies">Фильмы</Link></li>
             <li className='nav__item-log'><Link className='nav__link' to="/saved-movies">Сохранённые фильмы</Link></li>
             <li className='nav__item-log'><Link className='nav__link' to="/profile">Аккаунт</Link></li>
             <li className='nav__item-log'><button className='nav__button-log'></button></li>
+          </div>
+            <li><button className='burger__button' onClick={handelBurgerMenu}></button></li>
           </>
+
          :
           <>
             <li className='nav__item'><Link to="/signup" className='nav__link'> Регистрация</Link></li>
             <li className='nav__item'><Link to="/signin"><button className='nav__button'>Войти</button></Link></li>
           </>
         }
+
+        {isBurgerMenu ?
+          <div className='burger'>
+            <ul className='burger__list'>
+              <li className='burger__item'><Link className='nav__link' to="/">Главная</Link></li>
+              <li className='burger__item'><Link className='nav__link' to="/movies">Фильмы</Link></li>
+              <li className='burger__item'><Link className='nav__link' to="/saved-movies">Сохранённые фильмы</Link></li>
+              <li className='burger__item'><Link className='nav__link' to="/profile">Аккаунт</Link></li>
+            </ul>
+          </div>
+          : ""}
       </ul>
     </nav>
   )
